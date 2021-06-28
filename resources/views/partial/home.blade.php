85e2e6f5557ac98e9646c194a5996169da475ab2
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-06-28 15:13:33 +0200

    push exo yield et section et include

---
+++ resources/views/partial/home.blade.php
@@ -0,0 +1,15 @@
+@extends('index')
+
+
+@section('header')
+    @include('partial.header')
+@endsection
+
+@section('content')
+    <h1>My name is Charles de Lalaing</h1>
+    <p>yes</p>
+@endsection
+
+@section('footer')
+    @include('partial.footer')
+@endsection
